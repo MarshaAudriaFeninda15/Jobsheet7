5b251476ac34c886cab3c2850ac5cd377019541f
ASUS <ASUS@DESKTOP-3RT5GIB>
2018-09-11 06:09:40 +0700

    TambahanInput

--- src/DynamicPrograming/hitungFibonacci.java
+++ src/DynamicPrograming/hitungFibonacci.java
@@ -10,6 +10,14 @@ public class hitungFibonacci {
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
     }
+    private static int tampilInput() {
+            Scanner marsa = new Scanner(System.in);
+            
+            System.out.print("Bilangan ke-");
+           
+            int n = marsa.nextInt();
+            return n;
+        }
         
     
 }
