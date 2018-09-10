3c014040748068721ae7c036c1cddf076182dbdc
ASUS <ASUS@DESKTOP-3RT5GIB>
2018-09-11 06:06:26 +0700

    IntialCommit

---
+++ src/DynamicPrograming/hitungFibonacci.java
@@ -0,0 +1,15 @@
+
+package DynamicPrograming;
+import java.util.Scanner;
+import java.math.BigInteger;
+public class hitungFibonacci {
+    
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+        
+    
+}
